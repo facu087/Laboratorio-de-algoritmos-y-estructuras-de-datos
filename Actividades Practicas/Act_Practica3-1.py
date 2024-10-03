6b0403f4da6dd73add2d86563fd82bb855607906
Facundo Lusso <faculusso@gmail.com>
2024-10-03 15:12:49 -0300

    cambio brsuco

--- Actividades Practicas/Act_Practica3-1.py
+++ Actividades Practicas/Act_Practica3-1.py
@@ -140,3 +140,4 @@ print(frase)
 
 texto="Si la implementación es difícil de explicar, puede que sea una mala idea."
 print(texto.replace("difícil", "fácil").replace("mala", "buena"))
+dsdnkjasdsadasdass
