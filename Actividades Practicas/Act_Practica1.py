bcc60e21009b0d9ca594448952ea28d85af138e7
facu087 <faculusso@gmail.com>
2024-10-03 14:50:31 -0300

    inital commit

---
+++ Actividades Practicas/Act_Practica1.py
@@ -0,0 +1,15 @@
+# 1) Crea un código que imprima en pantalla la expresión 'Me encanta estudiar Python '
+print ("Me encanta estudiar Python")
+# 2) Crea un codigo que imprima la expresion 'Estudiar con "Python Total" es super divertido '
+print ("Estudiar con \"Python Total\" es super divertido")
+# 3) Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+print ("455+100")
+# 4) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿Qué estás estudiando? El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+print ("Estoy estudiando: " + input ("Que estas estudiando?")) 
+# 5) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿En qué país vives?
+print ("Vivo en : " + input ("Donde vivis?"))
+# 6) Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones: 
+# Escribe tu nombre:
+# Escribe tu apellido:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+print ("Tu nombre y apellido es : " + input("Escribe tu nombre") + " " + input ("Escribe tu apellido: ") )
